76230e91bace657fd13e967af056a29cf7c496bd
Krzysztof Graczyk <krzysztof.graczyk@uwr.edu.pl>
2023-11-02 19:02:56 +0100

    Create 2011-03-07-talk.m
    New conference

---
+++ _talks/2011-03-07-talk.m
@@ -0,0 +1,11 @@
+---
+title: "N-Delta Weak Transition"
+collection: talks
+type: "Talk"
+permalink: /talks/2011-03-07-talk
+venue: "Seventh International Workshop on Neutrino-Nucleus Interactions in the Few-GeV Region: NuInt11 conference"
+date: 2011-03-07-talk
+location: "07-11 March, 2011, Dehradun, India"
+---
+
+[More information here](http://nuint11.in/) 
